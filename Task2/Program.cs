1156592e28966958f94d110200fa530ba504d4ec
Maksim <mirror0791@mail.ru>
2023-08-07 22:28:49 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,5 @@
+﻿int number1 = new Random().Next(0, 999);
+int number2 = new Random().Next(0, 999);
+System.Console.WriteLine("Получим два рандомных чилса: " + number1 + ", " + number2);
+System.Console.WriteLine("Максимальное число: " + Math.Max(number1,number2));
+System.Console.WriteLine("Минимальное числож: " + Math.Min(number1,number2));

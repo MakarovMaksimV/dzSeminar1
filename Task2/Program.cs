ef38a153fe8adc5e9e5d7109b5ab68ce6588f7e9
Maksim <mirror0791@mail.ru>
2023-08-07 22:34:04 +0300

    Task2 Update 1.1

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,5 +1,16 @@
-﻿int number1 = new Random().Next(0, 999);
-int number2 = new Random().Next(0, 999);
-System.Console.WriteLine("Получим два рандомных чилса: " + number1 + ", " + number2);
-System.Console.WriteLine("Максимальное число: " + Math.Max(number1,number2));
-System.Console.WriteLine("Минимальное числож: " + Math.Min(number1,number2));
+﻿using System;
+
+class SEMINARDZ
+{
+    static void Main()
+    {
+        int number1 = new Random().Next(0, 999);
+        int number2 = new Random().Next(0, 999);
+        System.Console.WriteLine("Получим два рандомных чилса: " + number1 + ", " + number2);
+        System.Console.WriteLine("Максимальное число: " + Math.Max(number1,number2));
+        System.Console.WriteLine("Минимальное числож: " + Math.Min(number1,number2));
+    }
+
+}
+
+

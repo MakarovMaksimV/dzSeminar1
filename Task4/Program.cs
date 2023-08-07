08762d59bb6ce3eed7d4f1c66edff063fd85c5ef
Maksim <mirror0791@mail.ru>
2023-08-07 22:45:15 +0300

    Task4 Update

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SEMINARDZ
+{
+    class SEMINARDZ
+    {
+        static void Main()
+        {
+            int number1 = new Random().Next(0, 999);
+            int number2 = new Random().Next(0, 999);
+            int number3 = new Random().Next(0, 999);
+            System.Console.WriteLine("Получим два рандомных чилса: " + number1 + ", " + number2 + ", " + number3);
+            System.Console.WriteLine("Максимальное число: " + Math.Max(number1, Math.Max(number2, number3)));
+        }    
+    }
+
+    
+}    

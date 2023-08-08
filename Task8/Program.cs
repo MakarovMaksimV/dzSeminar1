f4cf0e8f44159d1b48fa0075e25e604bf5a26f2c
Maksim <mirror0791@mail.ru>
2023-08-08 21:20:22 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SEMINARDZ
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.Write("Введите целое число: ");
+            string NumberStr = Console.ReadLine();
+            int numberN = int.Parse(NumberStr);
+            int numberF = 1;
+            System.Console.WriteLine("Рандомное число: " + numberN);
+            if (numberN>0)
+            {
+                System.Console.WriteLine("Список целых чётных чисел до искомого числа включительно:");
+                while (numberF <= numberN)
+                {
+                    if (numberF > 0 & numberN > 0)
+                    {
+                        if (numberF %2 == 0)
+                        {
+                            System.Console.WriteLine(numberF);
+                        }   
+                    }
+                numberF ++;
+                }  
+            }
+            else
+            {
+                System.Console.WriteLine("Введите корректное число > 0");
+            }      
+        }    
+    }  
+}  
